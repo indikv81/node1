7cd1095df4a9f9b8b048a81cb9c003e8d2829792
Vyazovoy Denis <indikv@list.ru>
2021-10-01 00:18:34 +0400

    Второй коммит

--- Test1.js
+++ Test1.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 http.createServer(function (request, response) {
-    response.end("<b>Test 1</b>");
+    response.end("<b>Test 1 develop</b>");
 }).listen(3000,"127.0.0.1", function () {
     console.log("Сервер начал прослушивание запросов на порту 3000")
 });
